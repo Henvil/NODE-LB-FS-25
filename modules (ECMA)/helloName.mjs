265ee66d0013f9013fa7d190329d720d2a5cc91e
Henvil <turningsheeps@gmail.com>
2024-06-04 17:12:48 +0200

    modules ECMA done

---
+++ modules (ECMA)/helloName.mjs
@@ -0,0 +1,3 @@
+import printHello from "./printHello.mjs";
+
+printHello("Mario");
---
+++ modules (ECMA)/printHello.mjs
@@ -0,0 +1,5 @@
+function printHello(name) {
+  console.log(`Hello, my name is ${name}`);
+}
+
+export default printHello;
