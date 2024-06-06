72d8c5bb724e19c117328a58e44f1809714f86f2
Henvil <turningsheeps@gmail.com>
2024-06-07 00:16:12 +0200

    done exercise callbackHell.mjs

--- npm/callbackHell.mjs
+++ npm/callbackHell.mjs
@@ -1,7 +1,24 @@
 import * as fs from "node:fs";
+import { Buffer } from "node:buffer";
+
+const data = new Uint8Array(
+  Buffer.from("Sono stato generato dallo script e mi chiamo messaggio1.txt!")
+);
+fs.writeFile("messaggio1.txt", data, (error) => {
+  if (error) throw error;
+  console.log("Il file è stato salvato!");
+});
+
+const data2 = new Uint8Array(
+  Buffer.from("Sono stato generato dallo script e mi chiamo messaggio2.txt!")
+);
+fs.writeFile("messaggio2.txt", data2, (error) => {
+  if (error) throw error;
+  console.log("Il file è stato salvato!");
+});
 
 fs.readFile(
-  "fileLetturaEsercizio.txt",
+  "messaggio1.txt",
   { encoding: "utf-8" },
   function (error, file1Data) {
     if (error) {
@@ -12,7 +29,7 @@ fs.readFile(
      ${file1Data}`);
 
     fs.readFile(
-      "fileLetturaEsercizioCopia.txt",
+      "messaggio2.txt",
       { encoding: "utf-8" },
       function (error, file2Data) {
         if (error) {
